0e95b95ff7cf1bea759671fccc8af1d363c0b1fc
Musabbir-pr <musabbir313@gmail.com>
2023-02-17 10:23:11 +0600

    initialized app and added components

---
+++ public/Components/Routes/Routes.js
@@ -0,0 +1,9 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../Layout/Main");
+
+const router= createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>
+    }
+])
